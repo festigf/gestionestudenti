2bab3bcc12c0e37fa18fcb7c6d3430867c0ae37b
gianfranco festi <gianfranco.festi@marconirovereto.it>
2018-05-04 08:52:53 +0200

    Add utente component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UtenteComponent } from './utente/utente.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtenteComponent
   ],
   imports: [
     BrowserModule
